84e11de6754dabe93221e65e05803f6407e612f3
febriansy108-crypto <febriansy108@gmail.com>
2026-02-07 17:52:49 +0700

    Implement cleanup system for GUI elements
    Added a cleanup system to remove specific GUI elements from CoreGui.

--- FHub.lua
+++ FHub.lua
@@ -1,1 +1,46 @@
-p
+==============================
+-- CLEAN UP SYSTEM
+==============================
+if getgenv().fishingStart then
+    getgenv().fishingStart = false
+    task.wait(0.5)
+end
+
+local CoreGui = game:GetService("CoreGui")
+local GUI_NAMES = {
+    Main = "UQiLL_Fishing_UI",
+    Mobile = "UQiLL_Mobile_Button",
+    Coords = "UQiLL_Coords_HUD"
+}
+
+for _, v in pairs(CoreGui:GetChildren()) do
+    for _, name in pairs(GUI_NAMES) do
+        if v.Name == name then v:Destroy() end
+    end
+end
+
+for _, v in pairs(CoreGui:GetDescendants()) do
+    if v:IsA("TextLabel") and v.Text == "UQiLL" then
+        
+        local container = v
+        
+        for i = 1, 10 do
+            -- Cegah nil edge cases
+            if typeof(container) ~= "Instance" then 
+                break 
+            end
+
+            local parent = container.Parent
+            if not parent then 
+                break 
+            end
+
+            container = parent
+
+            if typeof(container) == "Instance" and container:IsA("ScreenGui") then
+                container:Destroy()
+                break
+            end
+        end
+    end
+end
