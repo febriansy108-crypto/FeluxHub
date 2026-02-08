ee2e8765a1bcc8e2f8fe85e431ba2789f4efcc68
Randhy <febriansy108@gmail.com>
2026-02-08 11:54:21 +0700

    Update FeluxHub.lua

--- FeluxHub.lua
+++ FeluxHub.lua
@@ -102,6 +102,22 @@ local PlayerGui = LocalPlayer:WaitForChild("PlayerGui")
 
 local WindUI = -
 -- =====================================================
--- CLEANUP SYSTEM
+-- UTILTY TELEPORT MENU
 -- =====================================================
-
+local Waypoints = {
+    ["Fisherman Island"]    = Vector3.new(-33, 10, 2770),
+    ["Traveling Merchant"]  = Vector3.new(-135, 2, 2764),
+    ["Kohana"]              = CFrame.new(-604, 2, 546) * CFrame.Angles(0, math.rad(90), 0),
+    ["Kohana Lava"]         = CFrame.new(-592, 59, 127) * CFrame.Angles(0, math.rad(75), 0),
+    ["Esoteric Island"]     = Vector3.new(1991, 6, 1390),
+    ["Esoteric Depths"]     = CFrame.new(3243, -1302, 1404) * CFrame.Angles(0, math.rad(160), 0),
+    ["Tropical Grove"]      = CFrame.new(-2136, 53, 3631) * CFrame.Angles(0, math.rad(120), 0),
+    ["Coral Reef"]          = Vector3.new(-3138, 4, 2132),
+    ["Weather Machine"]     = Vector3.new(-1517, 3, 1910),
+    ["Sisyphus Statue"]     = CFrame.new(-3657, -134, -963) * CFrame.Angles(0, math.rad(100), 0),
+    ["Treasure Room"]       = Vector3.new(-3599, -276, -1641),
+    ["Ancient Jungle"]      = CFrame.new(1483, 11, -302) * CFrame.Angles(0, math.rad(0), 0),
+    ["Ancient Ruin"]        = Vector3.new(6067, -586, 4714),
+    ["Sacred Temple"]       = Vector3.new(1498, -22, -640),
+    ["Crater Island"]       = CFrame.new(1015, 15, 5097) * CFrame.Angles(0, math.rad(140), 0),
+    ["Underground Cellar"]     = Vector3.new(2135, -91, -700),
